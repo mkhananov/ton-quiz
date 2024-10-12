1f063f5278f5dd9c109d92f631a51e2b7a514c11
mkhananov <mkhananov@yandex.ru>
2024-10-12 23:41:57 +0400

    some changes

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import basicSsl from "@vitejs/plugin-basic-ssl";
-// import ssr from "vite-plugin-ssr/plugin";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // plugins: [react(), ssr()],
   plugins: [react(), basicSsl()],
   build: {
-    outDir: "./docs",
+    outDir: "./build",
   },
   base: "./",
 });
